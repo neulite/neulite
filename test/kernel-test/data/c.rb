98609976090201953b5a4727a706846475feade7
neulite <neulite@numericalbrain.org>
2025-12-25 20:52:25 +0900

    Align with latest vanilla kernel.

--- test/kernel-test/data/c.rb
+++ test/kernel-test/data/c.rb
@@ -7,7 +7,7 @@ puts "#pre,post_i,post_c,weight,decay,rise,erev,delay,e/i"
 
 N.times{|i|
   N.times{|j|
-    weight = if i < E then 0.1 else 0.4 end
+    weight = if i < E then 0.1e-3 else 0.4e-3 end
     tau_decay = if i < E then 5 else 10 end
     tau_rise = 1
     erev = if i < E then 0.0 else -70.0 end
