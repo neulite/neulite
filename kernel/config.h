d4902922798d50fd94ebd2d9e48d8e1e926b8f8f
neulite <neulite@numericalbrain.org>
2026-01-02 22:29:42 +0900

    Updated to 1.0.2
    Aligned the default settings with Tutorial 1.

--- kernel/config.h
+++ kernel/config.h
@@ -5,15 +5,15 @@
 #undef DEBUG
 
 // Simulation parameters
-#define TSTOP ( 1000.0 )
+#define TSTOP ( 2000.0 )
 #define DT ( 0.1 )
 #define INV_DT ( ( int ) ( 1.0 / ( DT ) ) )
 
 // Neuron parameters
-#define SPIKE_THRESHOLD ( -20.0 )
+#define SPIKE_THRESHOLD ( -15.0 )
 #define ALLACTIVE ( 0 ) // Set to 1 for allactive models
 
 // Current injection parameters
-#define I_AMP ( 0.15 )
-#define I_DELAY ( 0.0 )
-#define I_DURATION ( 2000.0 )
+#define I_AMP ( 0.12 )
+#define I_DELAY ( 500.0 )
+#define I_DURATION ( 1000.0 )
