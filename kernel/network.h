45800a0f4e6d8ec5f818d9d955aa83100c098891
neulite <neulite@numericalbrain.org>
2026-01-02 22:31:48 +0900

    Updated to 1.0.2
    Implemented temporal blocking.

--- kernel/network.h
+++ kernel/network.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2024,2025 Neulite Core Team <neulite-core@numericalbrain.org>
+// Copyright (C) 2024,2025,2026 Neulite Core Team <neulite-core@numericalbrain.org>
 
 #pragma once
 
@@ -17,15 +17,11 @@ typedef struct {
   conn_t       *c;
   synapse_t    *s;
   FILE *v_dat, *s_dat;
-  double *v_prev;
   int *spike;
-  int tick, inv_dt;
 } network_t;
 
 extern network_t *initialize_network ( const char *, const char * );
 extern void finalize_network ( network_t * );
-extern void output_v ( const double, network_t * );
-extern void set_current ( const double, network_t *, double ( *current ) ( const double, const int ) );
-extern void solve_network ( network_t *, solver_t * );
-extern void spike_detection ( network_t * );
-extern void spike_propagation ( const double, network_t * );
+extern void set_current ( const int, network_t *, double ( *current ) ( const int, const int ) );
+extern void solve_network ( const int, network_t *, solver_t * );
+extern void spike_propagation ( const int, network_t * );
